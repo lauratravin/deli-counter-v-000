23003e88da6eb7d07e95b7fe20a67d1fb0d93c76
Laura Messa Travin <laura.travin@gmail.com>
2018-03-30 22:30:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,11 @@ def line(katz_deli)
   if katz_deli.empty?
      puts "The line is currently empty."
   elsif
-      i = 0
+      
       puts "The line is currently: "
        katz_deli.each do |name|
           puts "#{name}"
-          i += 1
+
         end
 
   end
